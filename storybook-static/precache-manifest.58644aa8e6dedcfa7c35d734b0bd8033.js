6de4fea3ee0b32273963641b21607150f8ca8164
Pedro <pedrobslisboa@gmail.com>
2020-09-11 14:48:12 -0300

    fix(readme): create readme

---
+++ storybook-static/precache-manifest.58644aa8e6dedcfa7c35d734b0bd8033.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231fac04c22b33061e18121972234b9a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cc25d627a4947f3cef74.bundle.js"
+  },
+  {
+    "url": "runtime~main.cc25d627a4947f3cef74.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc25d627a4947f3cef74.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc25d627a4947f3cef74.bundle.js.LICENSE.txt"
+  }
+]);
